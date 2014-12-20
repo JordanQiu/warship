f25ffccdc0ec94c57bea6dd3e1d6cb59046f5e68
JordanQiu <408017773@qq.com>
2014-12-20 20:59:50 +0800

    book service

---
+++ warship-service/src/main/java/org/sevenup/service/book/BookService.java
@@ -0,0 +1,13 @@
+package org.sevenup.service.book;
+
+import java.util.List;
+
+import org.sevenup.domain.book.Book;
+import org.springframework.dao.DataAccessException;
+
+public interface BookService {
+	List<Book> findBook() throws DataAccessException;
+
+   
+
+}
---
+++ warship-service/src/main/java/org/sevenup/service/book/BookServiceHandler.java
@@ -0,0 +1,27 @@
+package org.sevenup.service.book;
+
+import java.util.List;
+
+import org.sevenup.domain.book.Book;
+import org.sevenup.repository.book.BookRepository;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.Lists;
+
+@Service
+public class BookServiceHandler implements BookService {
+	private BookRepository bookRepository;
+
+	public BookServiceHandler(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+		bookRepository.createCollection();
+	}
+
+	@Override
+	public List<Book> findBook() throws DataAccessException {
+		List<Book> bookList = Lists.newArrayList(bookRepository.findAll());
+		return bookList;
+	}
+
+}
