6b861ae58220bcaa842ed06487f831fc07df6867
JordanQiu <408017773@qq.com>
2014-12-20 21:53:10 +0800

    gg

--- warship-rest/src/main/java/org/sevenup/rest/controller/book/BookController.java
+++ warship-rest/src/main/java/org/sevenup/rest/controller/book/BookController.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 	@Autowired
 	private BookService bookService;
-
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
+	@ResponseBody
 	public ResponseEntity<List<Book>> getBook() {
 		List<Book> bookList = bookService.findBook();
 		return new ResponseEntity<List<Book>>(bookList, HttpStatus.OK);
