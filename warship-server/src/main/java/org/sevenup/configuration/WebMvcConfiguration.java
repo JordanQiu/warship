3606b23d9a49bc342c899ccf019dc6f6d16ed00c
nonumber1989@gmail.com <nonumber7>
2014-12-20 17:44:42 +0800

    ok

--- warship-server/src/main/java/org/sevenup/configuration/WebMvcConfiguration.java
+++ warship-server/src/main/java/org/sevenup/configuration/WebMvcConfiguration.java
@@ -1,6 +1,10 @@
 package org.sevenup.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.repository.support.DomainClassConverter;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -8,5 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @EnableWebMvc
 @EnableSpringDataWebSupport
 public class WebMvcConfiguration extends WebMvcConfigurationSupport{
-
+	@Bean @Autowired
+	public DomainClassConverter domainClassConverter(ConversionService cs) {
+	    return new DomainClassConverter(cs);
+	}
 }
